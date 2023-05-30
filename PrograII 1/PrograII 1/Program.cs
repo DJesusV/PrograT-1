69ed1613ca9bcda48406d3b7e915674d830937c5
DJesusV <ed-dv@hotmail.com>
2023-05-30 12:48:30 -0600

    Tarea 1 Progra

---
+++ PrograII 1/PrograII 1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograII_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tipos de datos
+
+            byte b = 255; // 1 byte // Usa positivos
+
+            int num1 = 50; // 4 bytes
+            int num2, num3;
+            int i = 0;
+
+            float flotante = 33.3f;
+
+            double doble = 14.25555555555576d;
+
+            decimal deci = 158.366666666m;
+
+            string frase = "Hoy es viernes";
+            string nombre = null;
+
+            char caracter = 'A';
+
+            Boolean bolean = true;
+
+            var variable = 30;
+            variable = 20;
+
+            dynamic dinamico = "texto";
+            dinamico = 25;
+            dinamico = true;
+            dinamico = 256.322m;
+
+            Console.WriteLine("Digite su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Digite su edad");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mi nombre es:" + nombre + "y mi edad es:" + num1);
+            Console.ReadLine();
+
+            Console.WriteLine("la suma de dos numeros: " + (num1 + 10));
+            Console.WriteLine("la multiplicacion de dos numeros: " + (num1 * num2));
+            Console.WriteLine("la division de dos numeros: " + (num1 / num2));
+            Console.WriteLine("la resta de dos numeros: " + (num1 - num2));
+            Console.ReadLine();
+
+            // Contadores
+            i = i + 1;
+            i = i - 1;
+
+            i++;
+
+            // ciclos
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("Num1 es mayor");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("Num2 es mayor");
+            }
+            else
+            {
+                Console.WriteLine("Los numeros son iguales");
+            }
+
+            for (int j = 0; j < 10; j++) 
+            { 
+                Console.WriteLine(j); 
+            }
+            Console.ReadLine();
+
+            int x = 10;
+            while (x > 0)
+            {
+                Console.WriteLine(x);
+                x--;
+            }
+
+            char opcion = 's';
+            do
+            {
+                Console.WriteLine("1-sumar");
+                Console.WriteLine("1-restar");
+                Console.WriteLine("1-dividir");
+                Console.WriteLine("1-multiplicar");
+                Console.WriteLine("Digite una opcion... (s/n)");
+                opcion = char.Parse(Console.ReadLine());
+            } while (!opcion.Equals('n'));
+        }
+    }
+}
---
+++ PrograT 1/PrograT 1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograT_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("MENU");
+                Console.WriteLine("1. Sumar");
+                Console.WriteLine("2. Restar");
+                Console.WriteLine("3. Multiplicar");
+                Console.WriteLine("4. Dividir");
+                Console.WriteLine("5. Salir");
+                Console.WriteLine("Ingrese una opción (1-5):");
+
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Sumar();
+                        break;
+                    case 2:
+                        Restar();
+                        break;
+                    case 3:
+                        Multiplicar();
+                        break;
+                    case 4:
+                        Dividir();
+                        break;
+                    case 5:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opción inválida. Por favor, ingrese una opción válida (1-5).");
+                        break;
+                }
+
+                Console.WriteLine();
+
+            } while (opcion != 5);
+        }
+
+        static void Sumar()
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            double numero1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            double numero2 = double.Parse(Console.ReadLine());
+
+            double resultado = numero1 + numero2;
+
+            Console.WriteLine("El resultado de la suma es: " + resultado);
+        }
+
+        static void Restar()
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            double numero1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            double numero2 = double.Parse(Console.ReadLine());
+
+            double resultado = numero1 - numero2;
+
+            Console.WriteLine("El resultado de la resta es: " + resultado);
+        }
+
+        static void Multiplicar()
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            double numero1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            double numero2 = double.Parse(Console.ReadLine());
+
+            double resultado = numero1 * numero2;
+
+            Console.WriteLine("El resultado de la multiplicación es: " + resultado);
+        }
+
+        static void Dividir()
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            double numero1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            double numero2 = double.Parse(Console.ReadLine());
+
+            if (numero2 != 0)
+            {
+                double resultado = numero1 / numero2;
+                Console.WriteLine("El resultado de la división es: " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("No se puede dividir entre cero.");
+            }
+        }
+    }
+}
